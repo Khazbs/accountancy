9d99b661dd1dec32e660904cf4a62ffc22e347ef
Khazbs <khazbartur@yandex.ru>
2018-11-12 22:50:15 +0300

    Initial commit

---
+++ pay_salary.py
@@ -0,0 +1,9 @@
+from data import employees, salaries
+
+lupa = employees.get("Лупа")
+pupa = employees.get("Пупа")
+
+lupa_salary = salaries.get("Лупа")
+pupa_salary = salaries.get("Пупа")
+
+# TODO: pay salary
